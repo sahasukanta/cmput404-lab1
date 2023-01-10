9164952a8028192220238c7fc9ac5e2331ac8689
Sukanta Saha <sukanta@ualberta.ca>
2023-01-10 11:13:36 -0700

    uncommenting

--- find_version.py
+++ find_version.py
@@ -4,7 +4,7 @@ import requests
 print(requests.__version__)
 
 # get google homepage
-# print(requests.get("http://www.google.com").text)
+print(requests.get("http://www.google.com").text)
 
 
 print(requests.get("https://raw.githubusercontent.com/sahasukanta/cmput404-lab1/master/find_version.py").text)
